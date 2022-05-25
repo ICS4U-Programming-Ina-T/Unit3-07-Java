2feb8b5d2aec5819805254f366a661059bda84f5
Ubuntu <ubuntu@ip-172-31-49-244.ec2.internal>
2022-05-25 13:46:54 +0000

    Updated code.

--- Plane.java
+++ Plane.java
@@ -42,6 +42,70 @@ class Plane extends Vehicle {
     }
 
     /**
+     * Method that adds acceleration to the current speed.
+     *
+     * @param accelNum passed in
+     */
+    protected void accelerate(int accelNum) {
+        // adds acceleration
+        int newSpeed = _speed + accelNum;
+
+        // checks if speed is at max
+        if (_speed == _maxSpeed) {
+            System.out.println("Vehicle has reached its max speed.");
+        } else {
+            _speed = newSpeed;
+
+            // checks if speed is greater than max speed
+            if (_speed > _maxSpeed) {
+                _speed = _maxSpeed;
+            }
+
+            System.out.println("Vehicle has accelerated " + accelNum
+                + " km/h");
+        }
+    }
+
+    /**
+     * Method that alters the speed by applying brakes.
+     *
+     * @param brakeNum passed in
+     */
+    protected void brake(int brakeNum) {
+        // decreases amount of current speed
+        int newSpeed = _speed - brakeNum;
+
+        // checks if speed is at zero
+        if (_speed == 0) {
+            System.out.println("No change. Vehicle is at a speed of 0.");
+        } else {
+            _speed = newSpeed;
+
+            // checks to make sure speed is not negative
+            if (_speed < 0) {
+                _speed = 0;
+            }
+
+            System.out.println("A brake of "
+                + brakeNum + " km/h has been applied.");
+        }
+    }
+
+    /**
+     * Method that tells the user the current speed.
+     */
+    protected void getSpeed() {
+        System.out.println("Current speed: " + _speed + " km/h");
+    }
+
+    /**
+     * Method that tells the user what the max speed of a vehicle is.
+     */
+    protected void getMaxSpeed() {
+        System.out.println("Max speed: " + _maxSpeed + " km/h");
+    }
+
+    /**
      * Method that uses polymorphism to alter format of vehicle info.
      */
     protected void getInfo() {
--- Vehicle.java
+++ Vehicle.java
@@ -9,7 +9,7 @@
  * @since   2022-5-20
  */
 
-public class Vehicle {
+abstract public class Vehicle {
     /**
      * Property for _colour.
      */
@@ -66,76 +66,27 @@ public class Vehicle {
      *
      * @param accelNum passed in
      */
-    protected void accelerate(int accelNum) {
-        // adds acceleration
-        int newSpeed = _speed + accelNum;
-
-        // checks if speed is at max
-        if (_speed == _maxSpeed) {
-            System.out.println("Vehicle has reached its max speed.");
-        } else {
-            _speed = newSpeed;
-
-            // checks if speed is greater than max speed
-            if (_speed > _maxSpeed) {
-                _speed = _maxSpeed;
-            }
-
-            System.out.println("Vehicle has accelerated " + accelNum
-                + " km/h");
-        }
-    }
+    abstract void accelerate(int accelNum) {}
 
     /**
      * Method that alters the speed by applying brakes.
      *
      * @param brakeNum passed in
      */
-    protected void brake(int brakeNum) {
-        // decreases amount of current speed
-        int newSpeed = _speed - brakeNum;
-
-        // checks if speed is at zero
-        if (_speed == 0) {
-            System.out.println("No change. Vehicle is at a speed of 0.");
-        } else {
-            _speed = newSpeed;
-
-            // checks to make sure speed is not negative
-            if (_speed < 0) {
-                _speed = 0;
-            }
-
-            System.out.println("A brake of "
-                + brakeNum + " km/h has been applied.");
-        }
-    }
+    abstract void brake(int brakeNum) {}
 
     /**
      * Method that tells the user the current speed.
      */
-    protected void getSpeed() {
-        System.out.println("Current speed: " + _speed + " km/h");
-    }
+    abstract void getSpeed() {}
 
     /**
      * Method that tells the user what the max speed of a vehicle is.
      */
-    protected void getMaxSpeed() {
-        System.out.println("Max speed: " + _maxSpeed + " km/h");
-    }
+    abstract void getMaxSpeed() {}
 
     /**
      * Method that tells user information and characteristics about a vehicle.
      */
-    protected void getInfo() {
-        // applies appropriate format for information
-        System.out.println("License plate: " + _plateNum);
-        System.out.println("Colour: " + _colour);
-        System.out.println("Number of doors: " + _doorNum);
-        System.out.println("Number of seats: " + _seatNum);
-        System.out.println("Length: " + _length);
-        System.out.println("Speed: " + _speed + " km/h");
-        System.out.println("Max speed: " + _maxSpeed + " km/h");
-    }
+    abstract void getInfo() {}
 }
